9695d589b8e20807946e8b5e28e040892f17ade4
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-07-13 11:51:34 +0200

    refactor: improvements

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import { bootstrapApplication } from "@angular/platform-browser";
-import { AppComponent } from "./app.component";
 import { provideExperimentalZonelessChangeDetection } from "@angular/core";
-import { provideRouter } from "@angular/router";
-import { provideHttpClient } from "@angular/common/http";
+import { provideRouter, withRouterConfig } from "@angular/router";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { loggingInterceptor } from "$lib/interceptors/logging.interceptor";
+import { AppComponent } from "./app.component";
+import appRoutes from "./app.routes";
 
 bootstrapApplication(AppComponent, {
     providers: [
+        // Remove ZoneJs as Change Detection method
         provideExperimentalZonelessChangeDetection(),
-        provideHttpClient(),
-        provideRouter([
-            {
-                path: "",
-                loadChildren: () => import("./app.routes").then((r) => r.default),
-            },
-        ]),
+
+        // Provide the HttpClient application wide and add
+        // necessary interceptors
+        provideHttpClient(withInterceptors([loggingInterceptor])),
+
+        // Provide the base router, which just imports the created app Routes
+        // This enables separating out the App Routes. We can also
+        // provide the Router config here
+        provideRouter(appRoutes, withRouterConfig({ paramsInheritanceStrategy: "always" })),
     ],
 });
